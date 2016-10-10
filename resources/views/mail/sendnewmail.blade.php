362bee43dd5f15edcb279584a63ee10d8a79a293
root <root@60837.local>
2016-10-10 18:25:41 +0000

    Mail Form Validation  End

--- resources/views/mail/sendnewmail.blade.php
+++ resources/views/mail/sendnewmail.blade.php
@@ -49,5 +49,34 @@
 </div>
 
 
+<div class="container">
+	
+	<div class="row">
+		<div class="col-sm-12">
+			@if(Session::has('errors'))
+			@foreach ($errors->all() as $error) 
+			<div class="alert alert-danger">				
+				<button type="button" class="close" data-dismiss =
+				"alert" aria-hidden="true" > &times; </button>
+				<strong> {{ $error }}</strong>
+			</div>
+			@endforeach 
+
+			@endif
+		</div>		
+	</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 @stop
